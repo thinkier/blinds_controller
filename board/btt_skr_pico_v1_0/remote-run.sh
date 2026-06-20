7a7fe34600c2d0dc8c5e8f3d524d5d8d6e999a9b
thinkier <26728734+thinkier@users.noreply.github.com>
2026-06-20 23:36:46 +1000

    compile locally, run over ssh

---
+++ board/btt_skr_pico_v1_0/remote-run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+scp -i "$TARGET_IDENTITY" "$1" "$TARGET_HOST:/tmp/"
+EXECUTABLE_NAME="/tmp/${1##*/}"
+ssh -i "$TARGET_IDENTITY" "$TARGET_HOST" -- "probe-rs run --chip RP2040 '$EXECUTABLE_NAME'"
