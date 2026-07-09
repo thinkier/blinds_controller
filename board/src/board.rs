ae5e7d83f073112b0e7c03e04b1e8f33b159ddd4
thinkier <26728734+thinkier@users.noreply.github.com>
2026-07-09 11:58:24 +1000

    this should also work but it doesn't.

--- board/src/board.rs
+++ board/src/board.rs
@@ -190,8 +190,8 @@ impl BoardInitialize for Board<'static, 4, BufferedUart, HD, BttSkrPicoV1_0> {
 
 impl ControlLoopInvoke for BttSkrPicoV1_0 {
     async fn invoke(&mut self, _spawner: &mut Spawner) {
-        let temp = self.measure_temp();
-        debug!("Thermistor is at {}C", temp);
+        // let temp = self.measure_temp();
+        // debug!("Thermistor is at {}C", temp);
     }
 }
 
